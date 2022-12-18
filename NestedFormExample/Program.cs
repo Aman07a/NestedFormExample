1c90955924671646d47fa1c59aeda98afce9457f
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-18 10:22:11 +0100

    Nested For loops

--- NestedFormExample/Program.cs
+++ NestedFormExample/Program.cs
@@ -2,9 +2,19 @@
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			// 
+			for (int i = 1; i <= 5; i++)
+			{
+				for (int j = 1; j <= i; j++)
+				{
+					System.Console.Write(j);
+					System.Console.WriteLine("");
+				}
+				System.Console.WriteLine();
+			}
+
+			System.Console.ReadKey();
 		}
 	}
 }
